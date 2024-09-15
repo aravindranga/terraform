3c63555fbae0e5228e785a22fafbfec2b0d5840e
Aravind Ranga <aravind.ranga@gmail.com>
2024-09-15 12:53:15 +0530

    terraform

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,17 +1,17 @@
 resource "aws_instance" "terraform" {
 
-    ami = "ami-09c813fb71547fc4f"
+    ami = var.ami_id
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     
     tags = {
-        Name = "terraform"
+        Name = var.tags
     }
 }
 
 resource "aws_security_group" "allow_ssh_terraform" {
-    name        = "allow_ssssh" #allow_ssh is already there in my account
-    description ="Allow port number 22 for SSH access"
+    name        = var.sg_name #allow_ssh is already there in my account
+    description = var.sg_description 
 
     egress {
         from_port        = 0
@@ -22,16 +22,14 @@ resource "aws_security_group" "allow_ssh_terraform" {
     }
 
     ingress {
-        from_port        = 22
-        to_port          = 22
-        protocol         = "tcp"
-        cidr_blocks      = ["0.0.0.0/0"]
+        from_port        = var.from_port
+        to_port          = var.to_port
+        protocol         = var.protocol
+        cidr_blocks      = var.ingress_cidr
         ipv6_cidr_blocks = ["::/0"]
     }
 
-    tags = {
-    Name = "allow_ssssh"
-  }
+    tags = var.tags
 }
 
 # resource "aws_instance" "terraform" {

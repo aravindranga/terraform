799eb55c7912de35c47a8e0edb5e2c5ed1f6d59f
Aravind Ranga <aravind.ranga@gmail.com>
2024-09-15 13:00:21 +0530

    terraform

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -3,10 +3,8 @@ resource "aws_instance" "terraform" {
     ami = var.ami_id
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = var.tags
     
-    tags = {
-        Name = var.tags
-    }
 }
 
 resource "aws_security_group" "allow_ssh_terraform" {

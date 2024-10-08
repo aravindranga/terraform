b1f3ab4e0d759b93714c94785bbb3ce425681a1e
Aravind Ranga <aravind.ranga@gmail.com>
2024-10-08 22:05:25 +0530

    terraform

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,11 +1,11 @@
-# resource "aws_instance" "terraform" {
+resource "aws_instance" "terraform" {
 
-#     ami = var.ami_id
-#     instance_type = var.instance_type
-#     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-#     tags = var.tags
+    ami = var.ami_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = var.tags
     
-# }
+}
 
 resource "aws_security_group" "allow_ssh_terraform" {
     name        = var.sg_name #allow_ssh is already there in my account
